f3034b2d7a0aef4e9ae15a73363eb1434ed678ec
maxwellbenton <maxwellbenton@gmail.com>
2019-08-05 13:02:26 -0400

    adds folder to trigger code-along

---
+++ lib/example.rb
@@ -0,0 +1,13 @@
+# Use this space to code along with the readme
+
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
