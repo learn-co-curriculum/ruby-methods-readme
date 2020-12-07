29d09b10a93fa8d9c3301466e47dc657209be386
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-07 11:31:25 -0600

    commit tree 270e52e3f84272ada99707c5a0847fd5a4956389

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,17 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
