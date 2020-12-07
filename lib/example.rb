b6a023667977a7102fc815b718355675d53dce8e
busmother <75340481+busmother@users.noreply.github.com>
2020-12-07 17:45:48 -0500

    Update example.rb

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,9 @@
-# Use this space to code along with the readme
+def greeting
+    puts "Hello World"
+end
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+greeting
+greeting
+greeting
+greeting
+greeting
