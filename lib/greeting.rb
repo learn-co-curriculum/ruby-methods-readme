f321cf2f428c61852c76e29e9c1db1d8aa2648e3
Bill Miles <72408095+Willometers@users.noreply.github.com>
2020-12-08 14:32:30 -0500

    Create greeting.rb

---
+++ lib/greeting.rb
@@ -0,0 +1,15 @@
+
+def greeting
+    puts "Hello World!"
+    puts "Hello World!"
+    puts "Hello World!"
+    puts "Hello World!"
+    puts "Hello World!"
+    puts "Hello World!"
+    puts "Hello World!"
+    puts "Hello World!"
+    puts "Hello World!"
+    puts "Hello World!"
+end
+greeting
+
